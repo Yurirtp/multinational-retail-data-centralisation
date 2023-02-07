62beb456cc14d52d27adf4f361d1433427ac9744
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2023-02-07 17:38:24 +0000

    Update database_utils.py

--- MyDB/database_utils.py
+++ MyDB/database_utils.py
@@ -3,26 +3,32 @@ import yaml
 import sqlalchemy as db
 
 class DatabaseConnector:
-    def __init__(self):
+    def __init__(self, db_creds):
+        self.db_creds = db_creds
         self.creds = self.read_db_creds()
         self.engine = self.init_db_engine()
+        
 
     def read_db_creds(self):
-        with open("db_creds.yaml", "r") as stream:
+        with open(self.db_creds, "r") as stream:
             creds = yaml.safe_load(stream)
-        return creds
+        return creds    
 
     def init_db_engine(self):
-        credentials = f"postgresql://{self.creds['RDS_USER']}:{self.creds['RDS_PASSWORD']}@{self.creds['RDS_HOST']}:{self.creds['RDS_PORT']}/{self.creds['RDS_DATABASE']}"
+        creds = self.creds
+        credentials = f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
         engine = db.create_engine(credentials)
         return engine
 
     def list_db_tables(self):
-        metadata = db.MetaData()
-        tables = db.Table("", metadata, autoload=True, autoload_with=self.engine)
-        return tables.keys()
+        inspector = db.inspect(self.engine)
+        return inspector.get_table_names()
 
     def upload_to_db(self, df, table_name):
         df.to_sql(table_name, con=self.engine, if_exists="replace", index=False)
+
+
+db = DatabaseConnector("local_db.yaml")
+print(db.creds)
 
 
