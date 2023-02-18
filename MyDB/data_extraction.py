288dbd058944f3fb81f53b80abc42ef192b9dd80
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2023-02-18 16:18:06 +0000

    Update data_extraction.py

--- MyDB/data_extraction.py
+++ MyDB/data_extraction.py
@@ -7,14 +7,19 @@ class DataExtractor:
         self.connector = DatabaseConnector("db_creds.yaml")
 
     def extract_user_data(self):
-        table_name = "sales_data"
+        table_name = "legacy_users"
         df = self.read_rds_table(table_name)
         return df
+
+    #def retrieve_pdf_data(self, link):
+        #df = tabula.read_pdf(link, multiple_tables=True)
+        #return pd.concat(df)#
         
     def read_rds_table(self, table_name):
         query = f"SELECT * FROM {table_name}"
         df = pd.read_sql(query, con=self.connector.engine)
         return df
+
 
 
 
