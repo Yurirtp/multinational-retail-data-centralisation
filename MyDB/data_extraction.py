8d3abc9ba592a1037f01bcf87617e1bb216db5f1
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2023-02-18 22:44:08 +0000

    Update data_extraction.py

--- MyDB/data_extraction.py
+++ MyDB/data_extraction.py
@@ -3,22 +3,42 @@ import pandas as pd
 from database_utils import DatabaseConnector
 
 class DataExtractor:
-    def __init__(self):
-        self.connector = DatabaseConnector("db_creds.yaml")
+    def __init__(self, db_connector):
+        self.connector = db_connector
+
+    def read_rds_table(self, table_name):
+        query = f"SELECT * FROM {table_name}"
+        df = pd.read_sql(query, con=self.connector.engine)
+        return df
 
     def extract_user_data(self):
         table_name = "legacy_users"
         df = self.read_rds_table(table_name)
         return df
 
+# class DataExtractor:
+#     def __init__(self, db_connector):
+#         self.connector = db_connector
+#         db_connector = DatabaseConnector("db_creds.yaml")
+
+#     def read_rds_table(self, table_name):
+#         query = f"SELECT * FROM {table_name}"
+#         df = pd.read_sql(query, con=self.connector.engine)
+#         return df
+
+#     def extract_user_data(self):
+#         table_name = "legacy_users"
+#         df = self.read_rds_table(table_name)
+#         return df
+
     #def retrieve_pdf_data(self, link):
         #df = tabula.read_pdf(link, multiple_tables=True)
         #return pd.concat(df)#
         
-    def read_rds_table(self, table_name):
-        query = f"SELECT * FROM {table_name}"
-        df = pd.read_sql(query, con=self.connector.engine)
-        return df
+    # def read_rds_table(self, table_name):
+    #     query = f"SELECT * FROM {table_name}"
+    #     df = pd.read_sql(query, con=self.connector.engine)
+    #     return df
 
 
 
