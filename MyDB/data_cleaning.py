55ea7d74dfef9c10fdcea29114e9da10c412fff2
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2023-02-18 16:18:10 +0000

    Update data_cleaning.py

--- MyDB/data_cleaning.py
+++ MyDB/data_cleaning.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 class DataCleaning:
     def clean_user_data(self, df):
+        #cleaning user data, import data frame and do pandas cleaning
         # Perform data cleaning operations, such as handling missing values, converting data types, etc.
         df.dropna(inplace=True)
         df["date"] = pd.to_datetime(df["date"])
