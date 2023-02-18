d907f1f9ce651252fd8ea78f2054d3c10c1e5087
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2023-02-18 16:17:52 +0000

    Update main.py

--- MyDB/main.py
+++ MyDB/main.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     de = DataExtractor()
     original_df = de.extract_user_data()
 
-    # Step 2: Clean the data
+    #Step 2: Clean the data
     dc = DataCleaning()
-    cleaned_df = dc.clean_user_data(original_df)
+    print(original_df.head())
+    #cleaned_df = dc.clean_user_data(original_df)
 
     # Step 3: Upload the cleaned data to Sales_Data database
-    connector = DatabaseConnector("local_db.yaml")
-    connector.upload_to_db(cleaned_df, 'dim_users')
+    # connector = DatabaseConnector("local_db.yaml")
+    # connector.upload_to_db(cleaned_df, 'dim_users')
 
 
 # pip install --force-reinstall -v SQLAlchemy==1.4.46
